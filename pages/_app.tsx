4f2741ddb5f4683dfa655d0ad00fe30f3081144e
Michael Melis <michael.melis169@gmail.com>
2022-12-06 18:28:12 +0100

    feat Yup added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,18 @@
 import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { ThemeProvider as NextThemeProvider, useTheme } from 'next-themes';
+import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { appWithTranslation } from 'next-i18next';
-import { useMemo, useState, useEffect } from 'react';
-// import { PaletteMode } from '@mui/material';
 import createEmotionCache from '../config/createEmotionCache';
 import '../styles/globals.css';
 import store from '../store';
 import { MyAppProps } from '../@types/pages/app';
-import getDesignTokens from '../config/theme';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 const App = (props: MyAppProps) => {
-  const { theme: skin } = useTheme();
-  const [mode, setMode] = useState<any>('dark');
-
-  useEffect(() => {
-    setMode(skin);
-  }, []);
-
-  // const colorMode = useMemo(
-  //   () => ({
-  //     // The dark mode switch would invoke this method
-  //     toggleColorMode: () => {
-  //       setMode((prevMode: PaletteMode) => (prevMode === 'light' ? 'dark' : 'light'));
-  //     },
-  //   }),
-  //   [],
-  // );
-
-  // Update the theme only if the mode changes
-  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <NextThemeProvider enableSystem={false} attribute='class'>
@@ -47,11 +24,8 @@ const App = (props: MyAppProps) => {
               content='initial-scale=1, width=device-width'
             />
           </Head>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
+          <CssBaseline />
+          <Component {...pageProps} />
         </CacheProvider>
       </Provider>
     </NextThemeProvider>
